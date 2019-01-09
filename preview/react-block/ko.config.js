a253406636e3dc2630418a3cce36f986ff6ac81e
yangyi <yhxyangyi@163.com>
2019-01-09 10:50:09 +0800

    提交方式--update

--- preview/react-block/ko.config.js
+++ preview/react-block/ko.config.js
@@ -1,6 +1,6 @@
 
 const path = require('path');
-const isProd= process.env.NODE_ENV !='production';
+const isProd= process.env.NODE_ENV =='production';
 module.exports = (context) => {
   const { webpack } = context;
   return {
@@ -27,7 +27,6 @@ module.exports = (context) => {
           assets :path.resolve(__dirname,'public/assets'),
           layout:path.resolve(__dirname, 'src/layout'),
           components:path.resolve(__dirname, 'src/components')
-
         }
       }
     }
